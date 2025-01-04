96b40a522b31c4ca809436607794f50b6b2acb35
Axel Stage <aws-data-engineer@proton.me>
2025-01-04 19:59:30 +0100

    dev: rework airflow dag

--- airflow/dags/etl_cumulative_table.py
+++ airflow/dags/etl_cumulative_table.py
@@ -8,19 +8,21 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 0,
-    "execution_timeout": timedelta(minutes=3),
-    "depends_on_past": False,
+    "execution_timeout": timedelta(minutes=1),
     "wait_for_downstream": False,
+    "depends_on_past": True, # mandatory, keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded
 }
 
 with DAG(
   dag_id = "etl_cumulative_table",
+  description='etl process for daily sales data to build a cumulative table',
   start_date = datetime(2024,12,1),
+  max_active_runs = 1, # mandatory, parallel dag runs are not allowed
   catchup = True,
   schedule = "@daily",
   template_searchpath="/opt/airflow/query",
   default_args = default_args,
-  tags=["postgres", "test"]
+  tags=["postgres", "etl"]
 ) as dag:
 
   sales_daily_populate = SQLExecuteQueryOperator(
